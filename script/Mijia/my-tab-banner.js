2cede467cf34853d8626b61dafd4f2c28e36041d
QuentinHsu <xuquentinyang@gmail.com>
2022-05-09 14:47:26 +0800

    🐞 fix(Mijia): test

--- script/Mijia/my-tab-banner.js
+++ script/Mijia/my-tab-banner.js
@@ -3,12 +3,14 @@
 http-response ^https:\/\/home.\mi.\com\/cgi-op\/api\/v1\/recommendation\/myTab.*
 hostname = home.mi.com
 */
-$.log('🎉 米家-我的 去除 Banner 脚本--开始--执行')
-const obj = JSON.parse($response.body);
-$.log('obj.module.name', obj.module.name);
-obj.data.banners = []
-$done({body: JSON.stringify(obj)});
-$.log('🎉 米家-我的 去除 Banner 脚本--结束--执行')
+console.log('🎉 米家-我的 去除 Banner 脚本--开始--执行')
+let { body } = $response;
+const obj = JSON.parse(body);
+console.log('obj.module.name', obj.module.name);
+obj.data.banners = [];
+body = JSON.stringify(obj);
+$done({body});
+console.log('🎉 米家-我的 去除 Banner 脚本--结束--执行')
 // var body = $response.body;
 // var obj = JSON.parse(body);
 // delete obj.data;
