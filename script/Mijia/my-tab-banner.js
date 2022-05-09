4c3ff409ec73a9219e41614fe6ffaca6c2336718
QuentinHsu <xuquentinyang@gmail.com>
2022-05-09 13:51:53 +0800

    ✍️ change(Mijia): add console.log

--- script/Mijia/my-tab-banner.js
+++ script/Mijia/my-tab-banner.js
@@ -3,6 +3,8 @@
 http-response ^https:\/\/home.\mi.\com\/cgi-op\/api\/v1\/recommendation\/myTab.*
 hostname = home.mi.com
 */
-
+console.log('🎉 米家-我的 去除 Banner 脚本--开始--执行')
 const obj = JSON.parse($response.body);
+console.log('obj.module.name', obj.module.name);
 obj.data.banners = []
+console.log('🎉 米家-我的 去除 Banner 脚本--结束--执行')
