f19692c60ba0b4d0ebcae84e63343ca10b72507f
QuentinHsu <xuquentinyang@gmail.com>
2022-05-15 22:44:09 +0800

    ✍️ change(mijia): test

--- script/Mijia/find-tab-config.js
+++ script/Mijia/find-tab-config.js
@@ -1,8 +1,8 @@
 console.log("🎉 米家 - 发现页 -- 开始");
 let { body } = $response;
 const response = JSON.parse(body);
-response.data
-console.log("🚩[ response.data ]-5", response.data)
+console.log("[ response ]-5", JSON.stringify(response))
+console.log("🚩[ response.data.findTabSwitch ]-5", response.data.findTabSwitch)
 body = JSON.stringify(response);
 console.log("🎉 米家 - 发现页 -- 结束");
 $done({ body });
--- script/Mijia/my-tab-banner.js
+++ script/Mijia/my-tab-banner.js
@@ -1,13 +1,13 @@
 /*
-米家-我的 去除 Banner
+米家 - 我的 去除 Banner
 ^https:\/\/home\.mi\.com\/cgi-op\/api\/v1\/recommendation\/myTab
 hostname = home.mi.com
 */
-console.log("🎉 米家-我的 => 去除 Banner 脚本执行--开始");
+console.log("🎉 米家 - 我的 => 去除 Banner 脚本执行 -- 开始");
 let { body } = $response;
 const response = JSON.parse(body);
 response.data.banners = [];
 body = JSON.stringify(response);
-console.log("🎉 米家-我的 => 去除 Banner 脚本执行--结束");
+console.log("🎉 米家 - 我的 => 去除 Banner 脚本执行 -- 结束");
 $done({ body });
 // $done() 后的代码都不会执行
