b9a1ab879c26e2a502c87504908890250bec9231
QuentinHsu <xuquentinyang@gmail.com>
2022-05-16 15:45:05 +0800

    ✍️ change(mijia): modify body

--- script/Mijia/find-tab-config.js
+++ script/Mijia/find-tab-config.js
@@ -4,7 +4,8 @@ console.log(`$response.body -- ${$response.body}` )
 let { body } = $response;
 const response = JSON.parse(body);
 console.log(`🚩[ response.data.findTabSwitch ]", ${response.data.findTabSwitch}`)
-response.data.findTabSwitch = false;
+// response.data.findTabSwitch = false;
+response.data.findTabAccess.findTab = false;
 body = JSON.stringify(response);
 console.log("🎉 米家 - 发现页 -- 结束");
 $done({ body });
