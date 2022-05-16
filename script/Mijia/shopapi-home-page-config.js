83842196cc9025138f8d85016b76829af1f4a6e8
QuentinHsu <xuquentinyang@gmail.com>
2022-05-16 13:23:20 +0800

    ✍️ change(mijia): modify body

--- script/Mijia/shopapi-home-page-config.js
+++ script/Mijia/shopapi-home-page-config.js
@@ -1,7 +1,8 @@
 console.log("🎉 米家 -  shopapi-home-page-config -- 开始");
 let { body } = $response;
 const response = JSON.parse(body);
-response.data.default_unpacking = false
+// response.data.default_unpacking = false;
+response.data = {};
 body = JSON.stringify(response);
 console.log("🎉 米家 -  shopapi-home-page-config -- 结束");
 $done({ body });
