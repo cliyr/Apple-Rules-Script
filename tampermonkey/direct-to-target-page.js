5e0b1de42533c300848c6c4013f7bd6c327d49bc
QuentinHsu <xuquentinyang@gmail.com>
2022-05-23 10:24:14 +0800

    ✍️ change: update namespace

--- tampermonkey/direct-to-target-page.js
+++ tampermonkey/direct-to-target-page.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         直达目标页
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/QuentinHsu/Apple-Rules-Script
 // @version      1.0
 // @description  直达目标页，现支持掘金、简书、知乎
 // @author       QuentinHsu
