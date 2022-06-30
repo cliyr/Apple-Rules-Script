6eeaeceef2611c14fb8121fbf2f2a1892ce19c9a
QuentinHsu <xuquentinyang@gmail.com>
2022-06-30 19:45:58 +0800

    ✍️ change: reset

--- Stash/Script/my-ip-un-domestic.js
+++ Stash/Script/my-ip-un-domestic.js
@@ -1,20 +1,13 @@
-$httpClient
-  .get("http://ip-api.com/json/?lang=zh-CN", function (error, response, data) {
-    console.log("🎉国外 response data: " + data);
-    const dataObject = JSON.parse(data);
-    const { country, regionName, city, query } = dataObject;
-    const region = `地区：${country} ${regionName} ${city}`;
-    const ip = `IP：${query}`;
-    // 分割线
-    const line = "----";
-    // 国外 IP 信息
-    const infoFlag = `${line}国外${line}`;
-    const unDomesticInfo = `${infoFlag}\n${ip}\n${region}`;
-    console.log("🎉contentResult: " + contentResult);
-    $done({
-      title: "代理 IP 及所属地区",
-      content: `${unDomesticInfo}`,
-      backgroundColor: "#663399",
-      icon: "network",
-    });
+$httpClient.get("http://ip-api.com/json/?lang=zh-CN", function (error, response, data) {
+  const dataObject = JSON.parse(data);
+  const { country, regionName, city, query } = dataObject;
+  const region = `地区：${country} ${regionName} ${city}`;
+  const ip = `IP：${query}`;
+  const contentResult = `${ip}\n${region}`;
+  $done({
+    title: "当前 IP 及所属地区",
+    content: contentResult,
+    backgroundColor: "#663399",
+    icon: "network",
   })
+})
