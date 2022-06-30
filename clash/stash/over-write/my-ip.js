1f831cb7722c3b978380a7843853304434126d32
QuentinHsu <xuquentinyang@gmail.com>
2022-06-30 13:00:37 +0800

    ✍️ change: add log

--- clash/stash/over-write/my-ip.js
+++ clash/stash/over-write/my-ip.js
@@ -1,9 +1,11 @@
 $httpClient.get("http://ip-api.com/json/?lang=zh-CN", function (error, response, data) {
+  console.log('🎉response data: ' + data);
   const dataObject = JSON.parse(data);
   const { country, regionName, city, query } = dataObject;
   const region = `地区：${country} ${regionName} ${city}`;
   const ip = `IP：${query}`;
   const contentResult = `${ip}\n${region}`;
+  console.log('🎉contentResult: ' + contentResult);
   $done({
     title: "当前 IP 及所属地区",
     content: contentResult,
