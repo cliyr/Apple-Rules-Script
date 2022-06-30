fe2e5110b9b81ce9e10ee28df723a569c298f93f
QuentinHsu <xuquentinyang@gmail.com>
2022-06-30 21:15:17 +0800

    ✍️ change: clean

--- Stash/Script/my-ip-un-domestic.js
+++ Stash/Script/my-ip-un-domestic.js
@@ -1,18 +1,17 @@
-console.log('代理 IP 查询 ---- 开始');
+const hashLine = "###########"
+console.log(`${hashLine}🎉代理 IP 查询 ---- 开始${hashLine}`);
 $httpClient.get("http://ip-api.com/json/?lang=zh-CN", function (error, response, data) {
-  console.log("[🎉 dataObject ]" + data);
+  console.log("[🍎 response data ]" + '\n' + data);
   const dataObject = JSON.parse(data);
   let { country, countryCode, regionName, city, query } = dataObject;
-  console.log("[ country - old ]-5" + country);
   country = countryCode == "CN" ? "中国" : country;
-  console.log("[ country - new ]-6" + country)
   const region = `地区：${country} ${regionName} ${city}`;
   const ip = `IP：${query}`;
   const contentResult = `${ip}\n${region}`;
-  console.log("[ contentResult ]-7" + contentResult)
-  console.log('代理 IP 查询 ---- 结束')
+  console.log("[🍎 contentResult ]" + '\n'+ contentResult)
+  console.log(`${hashLine}🎉代理 IP 查询 ---- 结束${hashLine}`);
   $done({
-    title: "当前 IP 及所属地区",
+    title: "代理 IP 及所属地区",
     content: contentResult,
     backgroundColor: "#663399",
     icon: "network",
