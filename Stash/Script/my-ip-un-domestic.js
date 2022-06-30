aa56828ed0647f2b6b4df8d39795694146563766
QuentinHsu <xuquentinyang@gmail.com>
2022-06-30 20:40:23 +0800

    ✍️ change: test

--- Stash/Script/my-ip-un-domestic.js
+++ Stash/Script/my-ip-un-domestic.js
@@ -1,16 +1,18 @@
+console.log('代理 IP 查询 ---- 开始')
 $httpClient.get("http://ip-api.com/json/?lang=zh-CN", function (error, response, data) {
   console.log("[🎉 dataObject ]" + dataObject)
   const dataObject = JSON.parse(data);
   let { country, countryCode, regionName, city, query } = dataObject;
-  console.log("[ country - old ]-5" + country)
-  if(countryCode === "CN") {
-    country = "中国"
-  }
+  // console.log("[ country - old ]-5" + country)
+  // if(countryCode === "CN") {
+  //   country = "中国"
+  // }
   console.log("[ country - new ]-6" + country)
   const region = `地区：${country} ${regionName} ${city}`;
   const ip = `IP：${query}`;
   const contentResult = `${ip}\n${region}`;
   console.log("[ contentResult ]-7" + contentResult)
+  console.log('代理 IP 查询 ---- 结束')
   $done({
     title: "当前 IP 及所属地区",
     content: contentResult,
