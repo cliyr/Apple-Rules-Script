a64c0f47e17aaf0649a279c2d219adaed3a2cec7
QuentinHsu <xuquentinyang@gmail.com>
2022-06-30 15:50:28 +0800

    ✍️ change: test

--- clash/stash/over-write/my-un-domestic-ip.js
+++ clash/stash/over-write/my-un-domestic-ip.js
@@ -1,26 +1,31 @@
 /**
  * @description: 国外 IP 及所属地区
  */
- function getUnDomesticInfo() {
-  new Promise((resolve, reject) => {
-    $httpClient.get("http://ip-api.com/json/?lang=zh-CN", function (error, response, data) {
-      console.log('🎉国外 response data: ' + data);
-      const dataObject = JSON.parse(data);
-      const { country, regionName, city, query } = dataObject;
-      const region = `地区：${country} ${regionName} ${city}`;
-      const ip = `IP：${query}`;
-      // 分割线
-      const line = '----';
-      // 国外 IP 信息
-      const infoFlag = `${line}国外${line}`;
-      const unDomesticInfo = `${infoFlag}\n${ip}\n${region}`;
-      console.log("[ resolve unDomesticInfo ]-18" + unDomesticInfo)
-      resolve(unDomesticInfo);
-    }).catch(err => {
-      console.log('🎉国外 err: ' + err);
-      reject(err);
-    })
-  })
+function getUnDomesticInfo() {
+  return new Promise((resolve, reject) => {
+    $httpClient
+      .get(
+        "http://ip-api.com/json/?lang=zh-CN",
+        function (error, response, data) {
+          console.log("🎉国外 response data: " + data);
+          const dataObject = JSON.parse(data);
+          const { country, regionName, city, query } = dataObject;
+          const region = `地区：${country} ${regionName} ${city}`;
+          const ip = `IP：${query}`;
+          // 分割线
+          const line = "----";
+          // 国外 IP 信息
+          const infoFlag = `${line}国外${line}`;
+          const unDomesticInfo = `${infoFlag}\n${ip}\n${region}`;
+          console.log("[ resolve unDomesticInfo ]-18" + unDomesticInfo);
+          resolve(unDomesticInfo);
+        }
+      )
+      .catch((err) => {
+        console.log("🎉国外 err: " + err);
+        reject(err);
+      });
+  });
 }
 /**
  * @description: 整合信息，立即执行
@@ -28,12 +33,13 @@
 function getInfo() {
   const tileTemplate = {
     title: "当前国外 IP 及所属地区",
-    content: '',
+    content: "",
     backgroundColor: "#663399",
     icon: "network",
   };
-  getUnDomesticInfo().then(unDomesticInfo => {
-    console.log("[ domesticInfo ]-35" + unDomesticInfo)
+  console.log('当前国外 IP 及所属地区 --- 查询开始')
+  getUnDomesticInfo().then((unDomesticInfo) => {
+    console.log("[ domesticInfo ]-35" + unDomesticInfo);
     tileTemplate.content = unDomesticInfo;
     $done(tileTemplate);
   });
