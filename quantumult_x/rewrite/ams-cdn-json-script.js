b88a3d5d72fc2186ac82974f27d1ec20fca53ded
cliyr <364095354@163.com>
2025-03-19 11:34:23 +0800

    Create ams-cdn-json-script.js

---
+++ quantumult_x/rewrite/ams-cdn-json-script.js
@@ -0,0 +1,42 @@
+/**
+ * @fileoverview Example of HTTP rewrite.
+ *
+ * @supported Quantumult X (v1.0.5-build173)
+ */
+
+// $request, $response, $notify(title, subtitle, message), console.log(message)
+// $request.scheme, $request.method, $request.url, $request.path, $request.headers
+// $response.statusCode, $response.headers, $response.body
+//
+// $prefs is for persistent store and the data of $prefs will be cleared when Quantumult X is deleted.
+// $prefs.setValueForKey(value, key), $prefs.removeValueForKey(key), $prefs.removeAllValues(). Returns true or false, value and key should be string.
+// $prefs.valueForKey(key) returns value.
+//
+// setTimeout(function() { console.log("abc"); }, 1000);
+//
+// You can optional change the response headers at the same time by using $done({body: modifiedBody, headers: modifiedHeaders}); only change the response headers is not allowed for script-response-body. The modifiedHeaders can be copied and modified from $response.headers, please do not change the content length, type and encoding field.
+// Response status can also be optional changed by using $done({body: modifiedBody, headers: modifiedHeaders, status: modifiedStatus}), the modifiedStatus should be like "HTTP/1.1 200 OK"
+
+var body = $response.body;
+try
+{
+  var json = JSON.parse(body);
+
+  // 获取数据
+  let data = json[0].content[1].data;
+  
+  json[0].content[1].data = data.filter(item => item.title !== "乘车码" && item.title !== "充值");
+  
+  navigationAll=json[0].navigationAll;
+  json[0].navigationAll=navigationAll.filter(item => item.functionId !== "home"&& item.functionId !== "message" && item.functionId !== "me");
+  
+  changedBody = JSON.stringify(obj);
+  
+  console.log(changedBody);
+}
+catch(ex)
+{
+   console.log("异常："+ex.message);
+}
+body={};
+$done(body);
